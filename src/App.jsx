58cc72c0b5b36c837307a9dcadfa9f84ded7af46
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-14 15:57:20 +0300

    start using mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import MyComponent from "components/MyComponent";
+import styled from '@emotion/styled';
+import { ThemeProvider } from "@emotion/react";
+// adjust alias here
+import { theme } from "./theme";
+import { Button } from "@mui/material";
+
+const StyledButton = styled(Button)(({ theme }) => ({
+  backgroundColor: theme.palette.customColors.accentColor,
+  color: theme.palette.customColors.mainFontColor,
+  '&:hover': {
+    backgroundColor: theme.palette.customColors.highlightColor,
+  },
+}));
 
 function App() {
   return (
-    <div>
-      <MyComponent />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <StyledButton variant="contained">Contained Button</StyledButton>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+  palette: {
+    customColors: {
+      mainBgColor: '#f0f0f0',
+      secondaryBgColor: '#e1e2e6',
+      mainFontColor: '#4a4a4a',
+      secondaryFontColor: '#787775',
+      accentColor: '#c4b3a9',
+      highlightColor: '#b0a296',
+    }
+  }
+});
