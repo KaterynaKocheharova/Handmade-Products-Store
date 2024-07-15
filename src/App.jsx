250390815be355482718ca33f636037d296e13f8
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-15 17:27:33 +0300

    redux installed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { ThemeProvider } from "@emotion/react";
 // adjust alias here
 import { theme } from "./theme";
 import { Button } from "@mui/material";
+import FramerAnimation from './FramerAnimation';
+
+
 
 const StyledButton = styled(Button)(({ theme }) => ({
   backgroundColor: theme.palette.customColors.accentColor,
@@ -17,6 +20,7 @@ function App() {
     <ThemeProvider theme={theme}>
       <div>
         <StyledButton variant="contained">Contained Button</StyledButton>
+        <FramerAnimation/>
       </div>
     </ThemeProvider>
   );
