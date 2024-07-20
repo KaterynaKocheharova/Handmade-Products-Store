289531ae96dcd7ad9dae244e2adb1ec90a1a703f
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-21 00:39:06 +0300

    add hero button anim

--- src/components/structural/Hero/Hero.jsx
+++ src/components/structural/Hero/Hero.jsx
@@ -1,12 +1,12 @@
 import HeroTitle from "./HeroTitle/HeroTitle";
-import { Link } from "react-router-dom";
+import HeroButton from "./HeroButton/HeroButton";
 import css from "./Hero.module.css";
 
 const Hero = () => {
   return (
     <div className={css["hero-text-and-btn-container"]}>
       <HeroTitle />
-      <Link className={css["hero-catalog-button"]}>КАТАЛОГ</Link>
+      <HeroButton />
     </div>
   );
 };
---
+++ src/components/structural/Hero/HeroButton/HeroButton.jsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import css from "./HeroButton.module.css";
+
+const MotionLink = motion(Link);
+
+// Extract the animation, reuse it in the header
+
+const HeroButton = () => {
+  return (
+    <MotionLink
+      className={css["hero-catalog-button"]}
+      animate={{
+        scale: [1, 1.2, 1],
+      }}
+      transition={{
+        duration: 5,
+        repeat: Infinity,
+        repeatType: "loop",
+        ease: "linear" 
+      }}
+    >
+      КАТАЛОГ
+    </MotionLink>
+  );
+};
+
+export default HeroButton;
