8b0b93792f781003ab34465fdaebb97c5c3b9686
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-26 05:52:59 +0300

    add effect to the hero swiper

--- src/components/HomePageComponents/Swiper/Swiper.jsx
+++ src/components/HomePageComponents/Swiper/Swiper.jsx
@@ -1,13 +1,17 @@
-import { Navigation } from "swiper/modules";
+import { Navigation, EffectCreative, EffectFade, EffectCube } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { useSelector } from "react-redux";
 import { selectAllProducts } from "../../../redux/products/selectors";
 import SwiperTextBox from "../SwiperTextBox/SwiperTextBox";
+
 import css from "./Swiper.module.css";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/effect-creative";
+import "swiper/css/effect-fade";
+import "swiper/css/effect-cube";
 
 const ImagesSwiper = () => {
   const slides = useSelector(selectAllProducts);
@@ -15,7 +19,26 @@ const ImagesSwiper = () => {
   return (
     <div className={css["product-swiper"]}>
       <Swiper
-        modules={[Navigation]}
+        effect={"cube"}
+        cubeEffect={{
+          shadow: true,
+          slideShadows: true,
+          shadowOffset: 20,
+          shadowScale: 0.94,
+        }}
+        // effect={"creative"}
+        // creativeEffect={{
+        //   prev: {
+        //     shadow: true,
+        //     translate: [0, 0, -400],
+        //   },
+        //   next: {
+        //     translate: ['100%', 0, 0],
+        //   },
+        // }}
+        grabCursor={true}
+        // effect={'fade'}
+        modules={[EffectCreative, EffectFade, EffectCube, Navigation]}
         spaceBetween={50}
         slidesPerView={1}
         navigation
