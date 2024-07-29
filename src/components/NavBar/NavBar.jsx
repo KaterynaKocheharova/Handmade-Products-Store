be8b61cfdf2e482a5b834c0965681016dbd9438e
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-29 14:18:15 +0300

    start making navbar and hero responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,38 +3,40 @@ import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import Container from "components/common/Container/Container";
 import Header from "./Header/Header";
 import Navigation from "./Navigation/Navigation";
-import MobileMenu from "./MobileMenu/MobileMenu";
 import AuthNav from "components/AuthNav/AuthNav";
 import UserMenu from "components/UserMenu/UserMenu";
+import MobileMenu from "./MobileMenu/MobileMenu";
 import Stack from "@mui/material/Stack";
-import { Typography } from "@mui/material";
-import logo from "../../assets/My_Assets/logo.jpg";
+// import logo from "../../assets/My_Assets/logo.jpg";
 import css from "./NavBar.module.css";
+import clsx from "clsx";
 
 const NavBar = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   return (
     <Header>
-      <Stack
-        justifyContent="center"
-        alignItems="center"
-        sx={{
-          padding: "10px",
-          backgroundColor: "var(--main-font-color)}",
-        }}
-      >
-        <p className={css.text}>Вітаємо у нашому магазині!</p>
-      </Stack>
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          sx={{
+            padding: "10px",
+            backgroundColor: "var(--main-font-color)}",
+            width: "100%",
+          }}
+        >
+          <p className={css.text}>Вітаємо у нашому магазині!</p>
+        </Stack>
       <Container>
         <Stack
           direction="row"
           justifyContent="space-between"
           alignItems="center"
           spacing={2}
+          paddingTop= "10px"
+          paddingBottom="10px"
         >
-          <Navigation />
-          {/* <img src={logo} alt="logo"/> */}
-          <MobileMenu />
+          <MobileMenu/>
+          <Navigation/>
           {isLoggedIn ? <UserMenu /> : <AuthNav />}
         </Stack>
       </Container>
@@ -48,11 +50,11 @@ const NavBar = () => {
           backgroundColor: "var(--main-font-color)}",
         }}
       >
-        <a className={css["contact-link"]} href="tel:+380123456789">
+        <a className={clsx(css["contact-link"], css["tel-link"])} href="tel:+380123456789">
           +380123456789
         </a>
         <a className={css["contact-link"]} href="mailto:example@gmail.com">
-        example@gmail.com
+          example@gmail.com
         </a>
         <a
           className={css["contact-link"]}
