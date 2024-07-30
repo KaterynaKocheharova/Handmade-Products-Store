10320c98b0dc8a3d7656880d92a6687c9e75bbc8
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-30 15:15:12 +0300

    make swiper btn disabled on the last or first slides

--- src/components/HomePageComponents/SwiperCustomNavBtns/SwiperCustomNavBtns.jsx
+++ src/components/HomePageComponents/SwiperCustomNavBtns/SwiperCustomNavBtns.jsx
@@ -1,31 +1,51 @@
+import { useEffect, useState } from "react";
 import { useSwiper } from "swiper/react";
 import { GrPrevious, GrNext } from "react-icons/gr";
 import clsx from "clsx";
 import css from "./SwiperCustomNavBtns.module.css";
 
-const SwiperCustomNavBtns = ({
-  disabledRight = false,
-  disabledLeft = false,
-  onClick,
-}) => {
+const SwiperCustomNavBtns = ({ onClick }) => {
   const swiper = useSwiper();
+
+  const [disabledRight, setDisabledRight] = useState(false);
+  const [disabledLeft, setDisabledLeft] = useState(false);
+
+  useEffect(() => {
+    const updateButtonState = () => {
+      setDisabledLeft(swiper.isBeginning);
+      setDisabledRight(swiper.isEnd);
+    };
+    if (swiper) {
+      updateButtonState();
+      swiper.on('slideChange', updateButtonState);
+      return () => {
+        swiper.off('slideChange', updateButtonState);
+      };
+    }
+  }, [swiper]);
+
+
   return (
     <div className={css["nav-btn-container"]}>
       <button
-        className={clsx(css.button, { [css["button-disabled"]]: disabledLeft })}
+        className={clsx(css.button, { [css.disabled]: disabledLeft })}
         onClick={() => {
-          onClick(), swiper.slidePrev();
+          if (!disabledLeft) {
+            onClick();
+            swiper.slidePrev();
+          }
         }}
-        disabled={disabledLeft}
       >
         <GrPrevious className={css.icon} />
       </button>
       <button
-        className={clsx(css.button, {
-          [css["button-disabled"]]: disabledRight,
-        })}
-        onClick={() => swiper.slideNext()}
-        disabled={disabledRight}
+        className={clsx(css.button, { [css.disabled]: disabledRight })}
+        onClick={() => {
+          if (!disabledRight) {
+            onClick();
+            swiper.slideNext();
+          }
+        }}
       >
         <GrNext className={css.icon} />
       </button>
