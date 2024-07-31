591025025319f496831101ab5e6bfbebc9217c1a
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-07-31 17:02:10 +0300

    memoize selectProductByCategory

--- src/pages/ProductCategoryPage/ProductCategoryPage.jsx
+++ src/pages/ProductCategoryPage/ProductCategoryPage.jsx
@@ -1,12 +1,11 @@
 import { useSelector } from "react-redux";
-import { getSelector } from "./ProductCategoryPage.helpers";
+import { selectProductsByCategory } from "../../redux/products/selectors";
 import ProductList from "components/Products/ProductList/ProductList";
 import Section from "components/common/Section/Section";
 import Container from "components/common/Container/Container";
 
 const ProductCategoryPage = ({ category }) => {
-  const selector = getSelector(category);
-  const products = useSelector(selector);
+  const products = useSelector(selectProductsByCategory(category));
   return (
     <Section>
       <Container>
