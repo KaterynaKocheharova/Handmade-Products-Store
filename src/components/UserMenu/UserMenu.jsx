30aded2a86a1a323ad21e80486f63766648a2b3e
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-08-02 16:14:06 +0300

    add badge with the number of fav items

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,17 +1,33 @@
 import { Stack, IconButton } from "@mui/material";
+import { Badge } from "@mui/base/Badge";
 import { NavLink } from "react-router-dom";
 import { CiLogout } from "react-icons/ci";
 import { CiShoppingCart } from "react-icons/ci";
 import { CiHeart } from "react-icons/ci";
 import { buildActiveIconLinkClassname } from "./buildClasses";
+import { useSelector } from "react-redux";
+import { selectFavoriteProductsIds } from "../../redux/products/selectors";
+
 import css from "./UserMenu.module.css";
 
 const UserMenu = () => {
+  const favIds = useSelector(selectFavoriteProductsIds);
+  const favNumber = favIds.length;
+  console.log(favNumber);
+
   return (
     <Stack direction="row" spacing={2} alignItems="center">
       <IconButton>
-        <NavLink className={({isActive}) => buildActiveIconLinkClassname(isActive)} to="/favoriteProductsPage">
-          <CiHeart className={css.icon} />
+        <NavLink
+          className={({ isActive }) => buildActiveIconLinkClassname(isActive)}
+          to="/favoriteProductsPage"
+        >
+          <Badge
+            badgeContent={favNumber}
+            slotProps={{ badge: { className: css.badge } }}
+          >
+            <CiHeart className={css.icon} />
+          </Badge>
         </NavLink>
       </IconButton>
       <NavLink>
