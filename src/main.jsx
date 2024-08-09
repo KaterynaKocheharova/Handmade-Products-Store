8af2e0c141a28381616f4ce376905a11a0b8b18a
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-08-09 17:48:34 +0300

    ASDFG

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store.js";
+import { store, persistor } from "./redux/store.ts";
 import { PersistGate } from "redux-persist/integration/react";
 import "modern-normalize";
 import App from "./App.jsx";
