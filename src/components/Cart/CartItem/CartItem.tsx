e77ddef5f48256b12d6376763ca39e1dec88903d
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-08-21 14:34:51 +0300

    build cart item with mui grid

--- src/components/Cart/CartItem/CartItem.tsx
+++ src/components/Cart/CartItem/CartItem.tsx
@@ -1,13 +1,21 @@
+import { Grid, styled } from "@mui/material";
+import QuantityControl from "../../common/QuantityControl/QuantityControl";
+import css from "./CartItem.module.css";
 import { useAppSelector } from "../../../redux/hooks";
 import { selectProductById } from "../../../redux/products/selectors";
 import { type CartItem } from "../../../redux/cart/cartSlice";
-import { Typography, Stack } from "@mui/material";
-import QuantityControl from "../../common/QuantityControl/QuantityControl";
-import css from "./CartItem.module.css";
 
 type CartItemProps = {
   cartItemData: CartItem;
 };
+
+const StyledGridItem = styled(Grid)(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  padding: 0,
+}));
+
 const CartItem = ({ cartItemData: { productId, quantity } }: CartItemProps) => {
   const currentProduct = useAppSelector(selectProductById(productId));
 
@@ -18,23 +26,27 @@ const CartItem = ({ cartItemData: { productId, quantity } }: CartItemProps) => {
   const { image, name, new_price } = currentProduct;
 
   return (
-    <Stack
-      component="li"
-      direction="row"
-      spacing={2}
+    <Grid
+      container
+      rowSpacing={1}
+      columnSpacing={1}
+      justifyContent="center"
       alignItems="center"
       className={css["cart-item"]}
-      useFlexGap
-      flexWrap="wrap"
-      justifyContent="center"
     >
-      <img src={image} alt={name} width="80" className={css.image} />
-      <h3 className={css.name}>
-        {name}
-      </h3>
-      <p className={css.price}>{new_price * quantity} грн</p>
-      <QuantityControl productId={productId} />
-    </Stack>
+      <StyledGridItem item xs={6}>
+        <img src={image} alt={name} width="80" className={css.image} />
+      </StyledGridItem>
+      <StyledGridItem item xs={6}>
+        <h3 className={css.name}>{name}</h3>
+      </StyledGridItem>
+      <StyledGridItem item xs={6}>
+        <p className={css.price}>{new_price * quantity} грн</p>
+      </StyledGridItem>
+      <StyledGridItem item xs={6}>
+        <QuantityControl productId={productId} />
+      </StyledGridItem>
+    </Grid>
   );
 };
 
