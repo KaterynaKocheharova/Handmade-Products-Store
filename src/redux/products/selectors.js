4ca6331b306ad661f63285211a513219631e7442
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-09-17 15:46:27 +0300

    implement filtration by color

--- src/redux/products/selectors.js
+++ src/redux/products/selectors.js
@@ -6,20 +6,20 @@ export const selectAllProducts = (state) => state.products.items;
 export const selectProductById = (productId) => (state) =>
   state.products.items.find((item) => item.id === productId);
 
-export const selectProductsByCategory = (category) =>
-  createSelector([selectAllProducts], (products) =>
-    products.filter((product) => product.category === category)
-  );
-
-export const selectFilteredProducts = createSelector(
-  [selectProductsByCategory, selectAllProductFilters],
-  (products, filters) => {
-    if (filters.selectedColors.length >= 0) {
-      return products.filter((product) => {
-        product.colors.filter((color) =>
-          filters.selectedColors.includes(color)
+  export const selectProductsByCategory = (category) =>
+    createSelector(
+      [selectAllProducts, selectAllProductFilters],
+      (products, filters) => {
+        const categoryProducts = products.filter(
+          (product) => product.category === category
         );
-      });
-    }
-  }
-);
+        if (!filters.selectedColors.length) {
+          return categoryProducts;
+        } else {
+          return categoryProducts.filter((product) =>
+            product.colors.some((color) => filters.selectedColors.includes(color))
+          );
+        }
+      }
+    );
+  
