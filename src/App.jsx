c75d3cf8f47dc53d495291eb01a9adfc065dc4da
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-09-19 21:04:30 +0300

    fix home route

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ export default function App() {
             <Suspense fallback={<p>Loading...</p>}>
               <Routes>
                 <Route path="/" element={<Layout />}>
-                  <Route index element={<HomePage />} />
+                  <Route path="/home" element={<HomePage />} />
                   <Route
                     path="products/:category"
                     element={<ProductCategoryPage />}
